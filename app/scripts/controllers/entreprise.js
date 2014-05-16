a067b5ffed6c5c1ecc3a0de55d0dc85dc69830af
wab <wab2com@gmail.com>
2014-05-16 18:29:04 +0200

    developpement page entreprise

--- app/scripts/controllers/entreprise.js
+++ app/scripts/controllers/entreprise.js
@@ -1,9 +1,30 @@
 'use strict';
 
-cartonnApp.controller('EntrepriseCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+cartonnApp
+.controller('EntrepriseCtrl', ['$scope', '$http', function ($scope, $http) {
 	$http.get('contenu.json').success(function(data) {
-			$scope.page = data.entreprise;		
+			$scope.page = data.entreprise;
 		});
-	$scope.slickConfig = {};
-	$scope.slickHandle = {};
+	$scope.slickConfig = {
+
+		arrows: false
+
+	};
+
+	$scope.slickHandle = {
+
+	};
+
+	// classe active
+    $scope.islideActive = function(slide, scope, element, attrs) {
+
+    var activeSlide = angular.element('.slick-active').attr('data-id');
+    $scope.active = activeSlide;
+    	
+      if (slide.id === activeSlide) {
+        return true;
+      }
+      return false;
+    };
+
 }]);
