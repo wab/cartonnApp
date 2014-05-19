5fe26a0b9b160f462eaa0e8e926a471fda2b983e
wab <wab2com@gmail.com>
2014-05-19 12:55:17 +0200

    fonctionnement des videos dans la page clients

--- app/scripts/controllers/clients.js
+++ app/scripts/controllers/clients.js
@@ -4,6 +4,8 @@ cartonnApp
 .controller('ClientsCtrl', ['$scope', '$http', function ($scope, $http) {
 	$http.get('contenus/pages.json').success(function(data) {
 			$scope.page = data.clients;
+			$scope.videos = data.clients.videos;
+			$scope.mainVideoFile = data.clients.videos[1].file;
 		});
 	$scope.slickConfig = {
 
@@ -15,16 +17,22 @@ cartonnApp
 
 	};
 
+	// load video on click
+	$scope.loadvideo = function(videoFile) {
+		$scope.mainVideoFile = videoFile;
+	}
+
+	
 	// classe active
     $scope.islideActive = function(slide, scope, element, attrs) {
 
     var activeSlide = angular.element('.slick-active').attr('data-id');
     $scope.active = activeSlide;
     	
-      if (slide.id === activeSlide) {
-        return true;
-      }
-      return false;
+		if (slide.id === activeSlide) {
+	    	return true;
+	    	}
+	    return false;
     };
 
 }]);
