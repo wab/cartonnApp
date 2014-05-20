563beeeff78d66caa860d4019f88dc67e191ebff
wab <wab2com@gmail.com>
2014-05-20 13:18:52 +0200

    intégration page outil

--- app/scripts/controllers/outil.js
+++ app/scripts/controllers/outil.js
@@ -5,26 +5,5 @@ cartonnApp
 	$http.get('contenus/pages.json').success(function(data) {
 			$scope.page = data.outil;
 		});
-	$scope.slickConfig = {
-
-		arrows: false
-
-	};
-
-	$scope.slickHandle = {
-
-	};
-
-	// classe active
-    $scope.islideActive = function(slide, scope, element, attrs) {
-
-    var activeSlide = angular.element('.slick-active').attr('data-id');
-    $scope.active = activeSlide;
-    	
-      if (slide.id === activeSlide) {
-        return true;
-      }
-      return false;
-    };
 
 }]);
